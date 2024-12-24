f06a18cff51dfa39c34fc7023dd875f64ea258a1
Dharshini-V-hcl <dharshini-v@hcltech.com>
2024-12-24 08:32:37 +0530

    add

---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "nginx" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  security_groups = [aws_security_group.nginx_sg.name]
+
+  tags = {
+    Name = "nginx-instance"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "[nginx]" > ../ansible/inventory
+      echo "${self.public_ip} ansible_user=ubuntu ansible_private_key_file=../terraform/key-pair.pem" >> ../ansible/inventory
+    EOT
+  }
+}
+
+resource "aws_security_group" "nginx_sg" {
+  name        = "nginx-sg"
+  description = "Allow HTTP and SSH access"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value       = aws_instance.nginx.public_ip
+  description = "Public IP of the Nginx server"
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "ami" {
+  default = "ami-01816d07b1128cd2d"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  default = "my-key-pair"
+}
+
